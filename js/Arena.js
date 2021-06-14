28e18786c85c181588571f3178a366d4135dbb3b
ludovicmantovani <ludovic.mantov@gmail.com>
2021-06-15 01:34:52 +0200

    Remove sphere and ground objects

--- js/Arena.js
+++ js/Arena.js
@@ -4,9 +4,4 @@ Arena = function(game) {
 
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-    sphere.position.y = 1;
-
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
 };
