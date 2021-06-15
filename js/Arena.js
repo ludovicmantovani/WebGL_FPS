7ecf0e7d4abd7c1cb3c7d4747535f4f5f2a74c3f
ludovicmantovani <ludovic.mantov@gmail.com>
2021-06-15 02:09:45 +0200

    Add basic meshes

--- js/Arena.js
+++ js/Arena.js
@@ -4,4 +4,30 @@ Arena = function(game) {
 
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
+    var ground = BABYLON.Mesh.CreateGround("ground1", 20, 20, 2, scene);
+    ground.scaling = new BABYLON.Vector3(2,10,2);
+
+    var mainBox = BABYLON.Mesh.CreateBox("box1", 3, scene);
+    
+    mainBox.scaling.y = 1;
+    mainBox.position = new BABYLON.Vector3(
+        5,
+        ((3/2)*mainBox.scaling.y),
+        5);
+        mainBox.rotation.y = (Math.PI*45)/180;
+    
+    var mainBox2 = mainBox.clone("box2");
+    mainBox2.scaling.y = 2;
+    mainBox2.position = new BABYLON.Vector3(5,((3/2)*mainBox2.scaling.y),-5);
+
+    var mainBox3 = mainBox.clone("box3");
+    mainBox3.scaling.y = 3;
+    mainBox3.position = new BABYLON.Vector3(-5,((3/2)*mainBox3.scaling.y),-5);
+
+    var mainBox4 = mainBox.clone("box4");
+    mainBox4.scaling.y = 4;
+    mainBox4.position = new BABYLON.Vector3(-5,((3/2)*mainBox4.scaling.y),5);
+
+    var cylinder = BABYLON.Mesh.CreateCylinder("cyl1", 20, 5, 5, 20, 4, scene);
+    cylinder.position.y = 20/2;
 };
